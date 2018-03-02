98a23f153bb4babc026efc658c4fa48037fc7a9f
pq <pq.blogspot@gmail.com>
2018-03-02 21:21:56 +0900

    HM

--- TCU/automationexample.py
+++ TCU/automationexample.py
@@ -1,50 +1,61 @@
 #!/opt/libreoffice5.4/program/python
 # -*- coding: utf-8 -*-
-import officehelper
-import traceback
-def connect():  # LibreOfficeをバックグラウンドで起動してコンポネントテクストを取得する。
-	ctx = None
-	try:
-		ctx = officehelper.bootstrap()
-		if ctx:
-			print("Connected to a running office ...")
-		return ctx
-	except:
-		pass
-	return None
-ctx = connect()
-if ctx:
-	smgr = ctx.getServiceManager()
-	print("Using remote servicemanager\n")
-	if not smgr:
-		print( "ERROR: no service manager" )
-		 
-# PyUnoComponentの実行
-try:	   
+import unohelper  # オートメーションには必須(必須なのはuno)。
+def macro():
+	ctx = XSCRIPTCONTEXT.getComponentContext()  # コンポーネントコンテクストの取得。
+	smgr = ctx.getServiceManager()  # サービスマネージャーの取得。 
 	tcu = smgr.createInstanceWithContext("pq.Tcu", ctx)  # サービス名か実装名でインスタンス化。
-
-# 	tcu.wtree(ctx)
-# 	print("\n".join(tcu.tree("com.sun.star.frame.Desktop")))
-# 	print("\n".join(tcu.tree("com.sun.star.table.CellAddressConversion")))  # Can not get TypeDescription object of com.sun.star.table.CellAddressConversion.
-
-	filteroptiondialog = smgr.createInstanceWithContext("com.sun.star.comp.GraphicExportDialog", ctx)  # UIコンポーネントをインスタンス化。
-# 	filteroptiondialog = smgr.createInstanceWithContext("com.sun.star.comp.Calc.FilterOptionsDialog", ctx)  # UIコンポーネントをインスタンス化。
-	
-	tcu.wtree(filteroptiondialog)
-
-
-except:
-	traceback.print_exc()
-
- 
-# soffice.binの終了処理。これをしないとLibreOfficeを起動できなくなる。
-desktop = smgr.createInstanceWithContext("com.sun.star.frame.Desktop", ctx)
-from com.sun.star.beans import PropertyValue
-prop = PropertyValue(Name="Hidden",Value=True)
-desktop.loadComponentFromURL("private:factory/swriter", "_blank", 0, (prop,))  # バックグラウンドでWriterのドキュメントを開く。
-terminated = desktop.terminate()  # LibreOfficeをデスクトップに展開していない時はエラーになる。
-if terminated:
-	print("\nThe Office has been terminated.")  # 未保存のドキュメントがないとき。
-else:
-	print("\nThe Office is still running. Someone else prevents termination.")  # 未保存のドキュメントがあってキャンセルボタンが押された時。
+	tcu.wtree(print("\n".join(tcu.tree(ctx))))
+g_exportedScripts = macro, #マクロセレクターに限定表示させる関数をタプルで指定。
+if __name__ == "__main__":  # オートメーションで実行するとき
+	def automation():  # オートメーションのためにglobalに出すのはこの関数のみにする。
+		import officehelper
+		from functools import wraps
+		import sys
+		from com.sun.star.beans import PropertyValue
+		from com.sun.star.script.provider import XScriptContext  
+		def connectOffice(func):  # funcの前後でOffice接続の処理
+			@wraps(func)
+			def wrapper():  # LibreOfficeをバックグラウンドで起動してコンポーネントテクストとサービスマネジャーを取得する。
+				try:
+					ctx = officehelper.bootstrap()  # コンポーネントコンテクストの取得。
+				except:
+					print("Could not establish a connection with a running office.", file=sys.stderr)
+					sys.exit()
+				print("Connected to a running office ...")
+				smgr = ctx.getServiceManager()  # サービスマネジャーの取得。
+				print("Using {} {}".format(*_getLOVersion(ctx, smgr)))  # LibreOfficeのバージョンを出力。
+				return func(ctx, smgr)  # 引数の関数の実行。
+			def _getLOVersion(ctx, smgr):  # LibreOfficeの名前とバージョンを返す。
+				cp = smgr.createInstanceWithContext('com.sun.star.configuration.ConfigurationProvider', ctx)
+				node = PropertyValue(Name = 'nodepath', Value = 'org.openoffice.Setup/Product' )  # share/registry/main.xcd内のノードパス。
+				ca = cp.createInstanceWithArguments('com.sun.star.configuration.ConfigurationAccess', (node,))
+				return ca.getPropertyValues(('ooName', 'ooSetupVersion'))  # LibreOfficeの名前とバージョンをタプルで返す。
+			return wrapper
+		@connectOffice  # createXSCRIPTCONTEXTの引数にctxとsmgrを渡すデコレータ。
+		def createXSCRIPTCONTEXT(ctx, smgr):  # XSCRIPTCONTEXTを生成。
+			class ScriptContext(unohelper.Base, XScriptContext):
+				def __init__(self, ctx):
+					self.ctx = ctx
+				def getComponentContext(self):
+					return self.ctx
+				def getDesktop(self):
+					return ctx.getByName('/singletons/com.sun.star.frame.theDesktop')  # com.sun.star.frame.Desktopはdeprecatedになっている。
+				def getDocument(self):
+					return self.getDesktop().getCurrentComponent()
+			return ScriptContext(ctx)  
+		XSCRIPTCONTEXT = createXSCRIPTCONTEXT()  # XSCRIPTCONTEXTの取得。
+		doc = XSCRIPTCONTEXT.getDocument()  # 現在開いているドキュメントを取得。
+		doctype = "scalc", "com.sun.star.sheet.SpreadsheetDocument"  # Calcドキュメントを開くとき。
+	#  doctype = "swriter", "com.sun.star.text.TextDocument"  # Writerドキュメントを開くとき。
+		if (doc is None) or (not doc.supportsService(doctype[1])):  # ドキュメントが取得できなかった時またはCalcドキュメントではない時
+			XSCRIPTCONTEXT.getDesktop().loadComponentFromURL("private:factory/{}".format(doctype[0]), "_blank", 0, ())  # ドキュメントを開く。ここでdocに代入してもドキュメントが開く前にmacro()が呼ばれてしまう。
+		flg = True
+		while flg:
+			doc = XSCRIPTCONTEXT.getDocument()  # 現在開いているドキュメントを取得。
+			if doc is not None:
+				flg = (not doc.supportsService(doctype[1]))  # ドキュメントタイプが確認できたらwhileを抜ける。
+		return XSCRIPTCONTEXT
+	XSCRIPTCONTEXT = automation()  # XSCRIPTCONTEXTを取得。 
+	macro()  # マクロの実行。
 	  
