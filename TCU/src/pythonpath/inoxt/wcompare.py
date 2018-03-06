b085a3a5a5d424136989b9fbda1ba6e5970535c8
pq <pq.blogspot@gmail.com>
2018-03-06 22:06:46 +0900

    HM

--- TCU/src/pythonpath/inoxt/wcompare.py
+++ TCU/src/pythonpath/inoxt/wcompare.py
@@ -372,5 +372,5 @@ def createStackConsumer(indent, css, fns, st_oms, st_omi, st_omp):
 					elif typcls==INTERFACE_ATTRIBUTE:  # アトリビュートのとき。
 						typ = format_type(j.Type.Name.replace(css, ""))  # 戻り値の型を取得。
 						branch.append("{}  {}".format(typ.rjust(m), j.MemberName.replace(css, "")))  # 型は最大文字数で右寄せにする。
-						fns["INTERFACE_METHOD"]("".join(branch))  # アトリビュートの行を出力。				
+						fns["INTERFACE_METHOD"]("".join(branch))  # アトリビュートの行を出力。			
 		return consumeStack
