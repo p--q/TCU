6706696edba83dacf02b24ce7baf66c2e3ab4bf9
pq <pq.blogspot@gmail.com>
2018-03-10 12:33:14 +0900

    HP

--- TCU/src/pythonpath/inoxt/component.py
+++ TCU/src/pythonpath/inoxt/component.py
@@ -86,7 +86,9 @@ class TreeCommand(unohelper.Base, XServiceInfo, XTcu, XContainerWindowEventHandl
 		createHtml(ctx, offline, outputs)  # ウェブブラウザに出力。
 def createHtml(ctx, offline, outputs):  # ウェブブラウザに出力。
 	outputs.append("</tt>")	
-	html = "<br/>".join(outputs).replace(" ", chr(0x00A0))  # 連続したスペースはブラウザで一つにされるのでノーブレークスペースに置換する。
+	html = "<br/>".join(outputs)
+	spaces = sorted(set(re.findall(r'\u0020{2,}', html)), reverse=True)  # 連続したスペースのリストを長いもの順に取得。
+	[html.replace(s, chr(0x00A0)*len(s)) for s in spaces]  # 連続したスペースはブラウザで一つにされるのでノーブレークスペースに置換する。
 	title = "TCU - Tree Command for UNO"
 	if offline:  # ローカルリファレンスを使うときはブラウザのセキュリティの制限のためにhtmlファイルを開くようにしないとローカルファイルが開けない。
 		pathsettingssingleton = ctx.getByName('/singletons/com.sun.star.util.thePathSettings')
