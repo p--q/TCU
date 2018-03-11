6aff0d03c1d78493d167948258fdefde558a0132
pq <pq.blogspot@gmail.com>
2018-03-11 22:54:29 +0900

    Elem class

--- TCU/tools/helper.py
+++ TCU/tools/helper.py
@@ -1,20 +1,15 @@
 #!/opt/libreoffice5.2/program/python
 # -*- coding: utf-8 -*-
 import xml.etree.ElementTree as ET
-class Elem(ET.Element):  
-    '''
-    キーワード引数textでテキストノードを付加するxml.etree.ElementTree.Element派生クラス。
-    '''
-    def __init__(self, tag, attrib={},  **kwargs):  # textキーワードは文字列のみしか受け取らない。  
-        if "text" in kwargs:  # textキーワードがあるとき
-            txt = kwargs["text"]
-            del kwargs["text"]  
-            super().__init__(tag, attrib, **kwargs)
-            self._text(txt)
-        else:
-            super().__init__(tag, attrib, **kwargs)
-    def _text(self, txt):
-        self.text = txt
+class Elem(ET.Element):  # xml.etree.ElementTree.Element派生クラス。
+	def __init__(self, tag, attrib={},  **kwargs):  # ET.Elementのアトリビュートのtextとtailはkwargsで渡す。
+		txt = kwargs.pop("text", None)
+		tail = kwargs.pop("tail", None)
+		super().__init__(tag, attrib, **kwargs)
+		if txt:
+			self.text = txt
+		if tail:
+			self.tail = tail
 class ElemProp(Elem):  # 値を持つpropノード
 	def __init__(self, name, txt):
 		super().__init__("prop", {'oor:name': name}) 
